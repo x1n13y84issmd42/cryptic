ee3afeee0a2eb71ad6ca24ac733f86fafe54403f
Eugene Merkulov <gaddeena@gmail.com>
2020-01-07 12:06:29 +0200

    Yo

--- creep/runes/runes.sh
+++ creep/runes/runes.sh
@@ -4,6 +4,25 @@ function runes.log {
 	_IFS=$IFS && IFS='' && echo -e "\e[35mrunes\e[0m" $@ >&2 && IFS=$_IFS
 }
 
+function runes.load {
+	local runesFile=.creep/.runess
+	local pubKeyFile=.creep/runes.public.key
+	local privKeyFile=.creep/runes.public.key
+	
+	if [[ -f $runesFile ]]; then
+		readarray RUNES < $runesFile
+
+		if [[ ! (-f $pubKeyFile && -f $privKeyFile) ]]; then
+			runes.log "You don't have any keys to encrypt your content with."
+			return 255
+		fi
+	else
+		runes.log "You don't have a runes file. Create a .creep/.runes files in your project root to start using the encryption."
+	fi
+
+	return 0
+}
+
 # Checks if the given path belongs to the runes file.
 function runes.isRune {
 	for RUNE in ${RUNES[@]}; do
