af0bc336e6609b56aa9ab57efc0a29d76f6e71a4
Eugene Merkulov <gaddeena@gmail.com>
2020-01-07 16:09:26 +0200

    Updates to creep/git.

--- creep/git.sh
+++ creep/git.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# A script-friendly git status output.
 function git.status {
 	IFS=$'\n'
 	git status --porcelain
+}
+
+# A list of files that have been changed in last commit.
+function git.changes {
+	git diff --name-only HEAD HEAD~1
 }
 
 function git.add {
