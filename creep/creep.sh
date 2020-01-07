56eaeb6268b07bbaadce78ee65d94d46be264a93
Eugene Merkulov <gaddeena@gmail.com>
2020-01-07 11:38:39 +0200

    Yo

--- creep/creep.sh
+++ creep/creep.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-source git.sh
-
-# Checks if a progrma is installed in the system.
-# For use in if conditions.
-function sys.Installed {
-	local prog=$(command -v $1)
-	if [[ -z $prog ]]; then
-		return 255
-	else
-		return 0
-	fi
-}
+source creep/git.sh
