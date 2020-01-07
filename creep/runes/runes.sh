32ab407670c8a352b99d883e0930fc0212b2e1f1
Eugene Merkulov <gaddeena@gmail.com>
2020-01-07 12:18:32 +0200

    Yo

--- creep/runes/runes.sh
+++ creep/runes/runes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function runes.log {
-	_IFS=$IFS && IFS='' && echo -e "\e[35mrunes\e[0m" $@ >&2 && IFS=$_IFS
+	_IFS=$IFS && IFS='' && echo -en "\e[35mrunes\e[0m" $@ >&2 && echo -en "$lcX\n" >&2 && IFS=$_IFS
 }
 
 function runes.load {
@@ -13,11 +13,12 @@ function runes.load {
 		readarray RUNES < $runesFile
 
 		if [[ ! (-f $pubKeyFile && -f $privKeyFile) ]]; then
-			runes.log "You don't have any keys to encrypt your content with."
+			runes.log "${lcErr}You don't have any keys to encrypt your content with."
+			runes.log "${lcHint}Create a pair of keys by running ${lcCmd}./creep/runes/keygen.sh."
 			return 255
 		fi
 	else
-		runes.log "You don't have a runes file. Create a .creep/.runes files in your project root to start using the encryption."
+		runes.log "You don't have a runes file. ${lcHint}Create a ${lcCmd}${runesFile}${lcHint} file in your project root to start using the encryption."
 	fi
 
 	return 0
